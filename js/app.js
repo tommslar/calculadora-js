ec8fe0742ffaa1c55f8c0220ad95e1f05de5f947
Tomas Larrauri <tomas.larrauri@melon.tech>
2018-07-18 21:36:46 -0300

    agregando animaciones y chequeos

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ var Calculadora = {
  	operandob:0,
  	operacion:'',
  	res:0,
- 	memoria:0,
+ 	memoria:0, // para permitir operaciones en cadena
  	borrarMemoria: function (){
  		memoria = 0;
  	},
@@ -34,8 +34,9 @@ var Calculadora = {
 		}
  	},
  	ochoDigitos: function(){
-		if (display.innerHTML.length = 8)
+		if (display.innerHTML.length = 8){
 			display.innerHTML = display.innerHTML.substring(0,8);
+		}
 	},
 	init: function(){
 		this.uno.onclick = function(e){
@@ -66,7 +67,7 @@ var Calculadora = {
   			Calculadora.borrarMemoria();
   			Calculadora.borrarCeroInicial();
       		display.innerHTML = display.innerHTML  + "5";
-      		Calculadora.ochoDigitos();
+      		Calculadora.ochoDigitos();;
   		}
   		this.seis.onclick = function(e){
   			Calculadora.borrarMemoria();
@@ -144,7 +145,7 @@ var Calculadora = {
   			if (memoria == 0){
   				operandob = display.innerHTML;
   			}
-      		if ((operandob == '') || (operacion == '')){ // si operandob o el operador es igual a '', se asigna 0 y se resetea la operacion 
+      		if ((operandob == '') || (operacion == '') || (operandoa == '')){ // si operandob o el operador es igual a '', se asigna 0 y se resetea la operacion 
       			operandoa = 0;
   				operandob = 0;
   				operacion = "";
@@ -165,14 +166,128 @@ var Calculadora = {
 			     res = parseFloat(operandoa) / parseFloat(operandob);
 			     break;
   			}
-  			operandoa = res; // para permitir operaciones en cadena
+  			operandoa = res; 
   			memoria = operandob;
   			display.innerHTML = res;
-  			Calculadora.ochoDigitos();
+  			if (display.innerHTML.length = 8){
+				display.innerHTML = display.innerHTML.substring(0,8);
+  			}
   		}
 
- 	}
+ 	},
+ 	animate: function(){
+		this.uno.onmousedown = function (e){
+			Calculadora.uno.style.webkitFilter = "brightness(10%)";
+		}
+		this.uno.onmouseup = function (e){
+			Calculadora.uno.style.webkitFilter = "brightness(100%)";
+		}
+		this.dos.onmousedown = function (e){
+			Calculadora.dos.style.webkitFilter = "brightness(10%)";
+		}
+		this.dos.onmouseup = function (e){
+			Calculadora.dos.style.webkitFilter = "brightness(100%)";
+		}
+		this.tres.onmousedown = function (e){
+			Calculadora.tres.style.webkitFilter = "brightness(10%)";
+		}
+		this.tres.onmouseup = function (e){
+			Calculadora.tres.style.webkitFilter = "brightness(100%)";
+		}
+		this.cuatro.onmousedown = function (e){
+			Calculadora.cuatro.style.webkitFilter = "brightness(10%)";
+		}
+		this.cuatro.onmouseup = function (e){
+			Calculadora.cuatro.style.webkitFilter = "brightness(100%)";
+		}
+		this.cinco.onmousedown = function (e){
+			Calculadora.cinco.style.webkitFilter = "brightness(10%)";
+		}
+		this.cinco.onmouseup = function (e){
+			Calculadora.cinco.style.webkitFilter = "brightness(100%)";
+		}
+		this.seis.onmousedown = function (e){
+			Calculadora.seis.style.webkitFilter = "brightness(10%)";
+		}
+		this.seis.onmouseup = function (e){
+			Calculadora.seis.style.webkitFilter = "brightness(100%)";
+		}
+		this.siete.onmousedown = function (e){
+			Calculadora.siete.style.webkitFilter = "brightness(10%)";
+		}
+		this.siete.onmouseup = function (e){
+			Calculadora.siete.style.webkitFilter = "brightness(100%)";
+		}
+		this.ocho.onmousedown = function (e){
+			Calculadora.ocho.style.webkitFilter = "brightness(10%)";
+		}
+		this.ocho.onmouseup = function (e){
+			Calculadora.ocho.style.webkitFilter = "brightness(100%)";
+		}
+		this.nueve.onmousedown = function (e){
+			Calculadora.nueve.style.webkitFilter = "brightness(10%)";
+		}
+		this.nueve.onmouseup = function (e){
+			Calculadora.nueve.style.webkitFilter = "brightness(100%)";
+		}
+		this.cero.onmousedown = function (e){
+			Calculadora.cero.style.webkitFilter = "brightness(10%)";
+		}
+		this.cero.onmouseup = function (e){
+			Calculadora.cero.style.webkitFilter = "brightness(100%)";
+		}
+		this.suma.onmousedown = function (e){
+			Calculadora.suma.style.webkitFilter = "brightness(10%)";
+		}
+		this.suma.onmouseup = function (e){
+			Calculadora.suma.style.webkitFilter = "brightness(100%)";
+		}
+		this.resta.onmousedown = function (e){
+			Calculadora.resta.style.webkitFilter = "brightness(10%)";
+		}
+		this.resta.onmouseup = function (e){
+			Calculadora.resta.style.webkitFilter = "brightness(100%)";
+		}
+		this.multiplicacion.onmousedown = function (e){
+			Calculadora.multiplicacion.style.webkitFilter = "brightness(10%)";
+		}
+		this.multiplicacion.onmouseup = function (e){
+			Calculadora.multiplicacion.style.webkitFilter = "brightness(100%)";
+		}
+		this.division.onmousedown = function (e){
+			Calculadora.division.style.webkitFilter = "brightness(10%)";
+		}
+		this.division.onmouseup = function (e){
+			Calculadora.division.style.webkitFilter = "brightness(100%)";
+		}
+		this.igual.onmousedown = function (e){
+			Calculadora.igual.style.webkitFilter = "brightness(10%)";
+		}
+		this.igual.onmouseup = function (e){
+			Calculadora.igual.style.webkitFilter = "brightness(100%)";
+		}
+		this.punto.onmousedown = function (e){
+			Calculadora.punto.style.webkitFilter = "brightness(10%)";
+		}
+		this.punto.onmouseup = function (e){
+			Calculadora.punto.style.webkitFilter = "brightness(100%)";
+		}
+		this.negativo.onmousedown = function (e){
+			Calculadora.negativo.style.webkitFilter = "brightness(10%)";
+		}
+		this.negativo.onmouseup = function (e){
+			Calculadora.negativo.style.webkitFilter = "brightness(100%)";
+		}
+		this.reset.onmousedown = function (e){
+			Calculadora.reset.style.webkitFilter = "brightness(10%)";
+		}
+		this.reset.onmouseup = function (e){
+			Calculadora.reset.style.webkitFilter = "brightness(100%)";
+		}
+
+	}
 }
 
 
 Calculadora.init();
+Calculadora.animate();
